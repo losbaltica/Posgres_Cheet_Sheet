821fa6afbe7c9b3b247918bf8603202b95e192f6
losbaltica <adammakota2@gmail.com>
2020-06-15 11:24:49 +0200

    Create Function_execution_time_wrapper.sql

---
+++ Function_execution_time_wrapper.sql
@@ -0,0 +1,22 @@
+--
+-- Name: nm_execution_time_raise(character varying); Type: FUNCTION; Schema: f_analysis; Owner: postgres
+--
+
+CREATE FUNCTION f_analysis.execution_time_raise(function_name character varying) RETURNS void
+    LANGUAGE plpgsql
+AS
+$$
+DECLARE
+    StartTime  timestamptz;
+    EndTime    timestamptz;
+    Delta      double precision;
+    TimeFormat varchar;
+BEGIN
+    StartTime := clock_timestamp();
+    EXECUTE function_name;
+    EndTime := clock_timestamp();
+    Delta := (extract(epoch from EndTime) - extract(epoch from StartTime));
+    TimeFormat := to_char(Delta, 'FM999999999.000');
+    RAISE NOTICE 'Function: % executed in % secs', function_name, TimeFormat;
+END
+$$;
