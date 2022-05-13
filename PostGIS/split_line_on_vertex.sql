325b2d0734239d0a09b035378a6473fb8ad50e7a
losbaltica <adammakota2@gmail.com>
2022-05-13 11:19:08 +0200

    Create split_line_on_vertex.sql

---
+++ PostGIS/split_line_on_vertex.sql
@@ -0,0 +1,11 @@
+// Jim Jones
+// https://stackoverflow.com/a/70516940/5935388
+
+CREATE TABLE split_line AS
+WITH j AS (
+  SELECT id, ST_MakeLine(j.geom,LEAD(j.geom) OVER w) AS line
+  FROM source_line, ST_DumpPoints(geom) j (path,geom)
+  WINDOW w AS (PARTITION BY id ORDER BY j.path
+               ROWS BETWEEN CURRENT ROW AND 1 FOLLOWING)
+)
+SELECT * FROM j;
